ec330177c3c2e7d468be3ced814f3c09fd1af3af
JoeKahora <gachau.joel@gmail.com>
2020-02-02 20:24:17 +0300

    Added my Api call from openweather

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function getCurrentWeather(){
-    alert("Test!")
+    var cityName = $("#searchplace").val();
+    var weatherApiCall = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&&appid=f11adde7d4f7b71c1c1be26bc839a821"
+    $.getJSON(weatherApiCall, weatherData)
+        function weatherData(currentWeatherData) {
+            var cityName = currentWeatherData.name
+            var countryName = currentWeatherData.sys.country
+            var temperature = currentWeatherData.main.temp
+            $("#output").append("<br>The weather at" + cityName + " in country code " + countryName + "<br>The Temperature there is " + temperature); 
+            console.log(currentWeatherData)           
+        }
 }
